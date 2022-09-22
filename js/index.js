7d06ddb837f0c28ac0131468fd75ab416c4f4bea
Aisar <ahmed.aisar@gmail.com>
2022-09-22 22:22:33 +0500

    update burn countdown

--- js/index.js
+++ js/index.js
@@ -1151,7 +1151,7 @@ async function getSupply() {
 }
 
 async function setBurnCountdown(){
-    var countDownDate = new Date("September 22 2022 12:01:00 GMT+05:00").getTime();
+    var countDownDate = new Date("September 23 2022 12:01:00 GMT+05:00").getTime();
     var x = setInterval(function () {
         var now = new Date().getTime();
         var distance = countDownDate - now;
